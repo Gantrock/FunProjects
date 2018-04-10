e709c8df2a23e82d06759ed634916a6a80bfc47d
Gantrock <Gantrock@users.noreply.github.com>
2018-04-10 15:19:55 -0700

    Update FizzBuzz.java
    Added description

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,5 +1,5 @@
 public class Fizzbuzz {
-
+//Counts to 100, if the number is divisble by three print fizz instead, if divisible by 5 print buzz. If divisible by both print fizzbuzz
 	public static void main(String[] args) {
 		for(int i = 1; i < 101; i++) {
 			System.out.println(fizzbuzz(i));
