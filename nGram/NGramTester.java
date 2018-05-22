ae684b9579e9d5a5845918bfb318f78913e0eb70
Gantrock <Gantrock@users.noreply.github.com>
2018-05-22 14:32:06 -0700

    Update NGramTester.java
    Added modularity

--- nGram/NGramTester.java
+++ nGram/NGramTester.java
@@ -4,13 +4,17 @@ import java.io.UnsupportedEncodingException;
 public class NGramTester {
 
 	public static void main(String[] args) {
+		//alter these two if you wanted to use your own files.
+		String fileName = "doyle-27.txt";
+		String secFilename = "doyle-case-27.txt";
+		
 		Unigram one = new unigram();
 		Bigram two = new Bigram();
 		Smoothed plusDelta = new Smoothed();
 		try {
-			one.probabilities();
-			two.probabilities();
-			plusDelta.probabilities();
+			one.probabilities(fileName, secFileName);
+			two.probabilities(fileName, secFileName);
+			plusDelta.probabilities(fileName, secFileName);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (UnsupportedEncodingException e) {
