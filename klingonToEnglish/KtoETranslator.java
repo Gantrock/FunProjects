d41153a89e16fad0abee7588047482d1aaf8edc9
Gantrock <Gantrock@users.noreply.github.com>
2018-05-22 20:54:27 -0700

    Create KtoETranslator.java
    Added class and comments

---
+++ klingonToEnglish/KtoETranslator.java
@@ -0,0 +1,18 @@
+public class KtoEMain {
+	//mughwI' = translator?
+	public static void main(String[] args) {
+		String filename = "Klingon_Train.txt";
+    //change target string below to test against a custom phrase/sentence
+		String target = "tera’ngan legh yaS";
+    //Creates the Emission table
+		Emission eTable = new Emission(filename);
+    //Creates the Transition table
+		Transition tTable = new Transition(filename);
+    //Uses the Emission and Transition tables to decode the phrase.
+		Decoding decoder = new Decoding(eTable, tTable);
+		decoder.decode(target);
+		System.out.println("Complete");
+
+	}
+
+}
